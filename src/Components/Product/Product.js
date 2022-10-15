f5328de128e4adf19def8b586f4c12652f5e2432
dev-rahim <rahimuddin5915@gmail.com>
2022-10-15 14:02:56 +0600

    added cart dynamically

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faArrowTrendDown } from '@fortawesome/free-solid-svg-icons'
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
 const Product = (props) => {
+    // const element = <FontAwesomeIcon icon={faArrowTrendDown} />
+    const element = <FontAwesomeIcon icon={faCartPlus} />
+
+
     const { name, seller, price, stock, img, } = props.product;
     // console.log();
     return (
@@ -13,7 +20,7 @@ const Product = (props) => {
                 <p>by: {seller}</p>
                 <h4>${price}</h4>
                 <p>only {stock} left in stock - order soon</p>
-                <button onClick={() => props.addToCartBtn(props.product)} className='btn-addToCart'> add to cart</button>
+                <button onClick={() => props.addToCartBtn(props.product)} className='btn-addToCart'> <span>{element} </span> add to cart</button>
             </div>
         </div>
     );
