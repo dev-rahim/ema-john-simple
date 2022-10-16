c3d6a7c375c62d0c7925617b6379dd0ba3fa8f8c
dev-rahim <rahimuddin5915@gmail.com>
2022-10-16 12:15:30 +0600

    added product data saved on local storage

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -3,23 +3,31 @@ import './Cart.css'
 const Cart = (props) => {
     // console.log(props);
     const { cart } = props;
+    console.log(cart);
+
+    let totalQuantity = 0;
     let total = 0;
     for (const product of cart) {
-        total = total + product.price;
+        product.quantity = !product.quantity ? 1 : product.quantity;
+        total = (total + product.price) * product.quantity;
+        totalQuantity += product.quantity;
     }
+
+    const shipping = total > 0 ? 15 : 0;
+    const tax = (total + shipping) * 0.10;
+    const grandTotal = total + shipping + tax;
     return (
         <div>
             <div className="cart-header">
                 <h2>Order Summary</h2>
-                <h4>Items ordered: {props.cart.length}</h4>
+                <h4>Items ordered: {totalQuantity}</h4>
             </div>
             <div className='cart-calculation'>
-                <p><span>Items:	</span><span>{total}</span></p>
-                <p><span>Shipping & Handling:	</span><span>{0}</span></p>
-                <p><span>Total before tax:	</span><span>{0}</span></p>
-                <p><span>Estimated Tax:	</span><span>{0}</span></p>
-                <h3><span>Order Total:	</span><span>${0}</span></h3>
-
+                <p><span>Total:	</span><span>${total.toFixed(2)}</span></p>
+                <p><span>Shipping & Handling:	</span><span>${shipping.toFixed(2)}</span></p>
+                <p><span>Total before tax:	</span><span>${(shipping + total).toFixed(2)}</span></p>
+                <p><span>Estimated Tax:	</span><span>${tax.toFixed(2)}</span></p>
+                <h3><span>Order Total:	</span><span>${grandTotal.toFixed(2)}</span></h3>
             </div>
         </div>
     );
--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Products.css'
@@ -6,12 +7,35 @@ import './Products.css'
 const Products = () => {
 
     const [products, setProducts] = useState([]);
-
+    // load data from api 
     useEffect(() => {
         fetch('./products.json')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
+
+    // get added cart items
+    useEffect(() => {
+        if (products.length) {
+            const savedCart = getStoredCart();
+            // console.log(savedCart);
+            const savedProduct = []
+            for (const key in savedCart) {
+                // console.log(key);
+                const addedProduct = products.find(product => product.key === key)
+                // console.log(addedProduct, key);
+                if (addedProduct) {
+                    // get keys value as quantity 
+                    const quantity = savedCart[key];
+                    // set object quantity
+                    addedProduct.quantity = quantity;
+                    savedProduct.push(addedProduct);
+
+                }
+            }
+            setCart(savedProduct);
+        }
+    }, [products])
 
     const [cart, setCart] = useState([])
     const handleAddToCart = (product) => {
@@ -19,6 +43,10 @@ const Products = () => {
         // setProduct(product + 1);
         // console.log(count);
         // console.log(product.name);
+
+        /* save to localStorage */
+        addToDb(product.key)
+
     }
     return (
         <div className='Products-container'>
