0468c034db44142db958525b380dd0b04238100e
dev-rahim <rahimuddin5915@gmail.com>
2022-10-22 23:17:09 +0600

    using stats in a diffrent foldar with a function for reuse it

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import useProducts from '../../hooks/useProducts';
 import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
