e6e3ff6e4ae422a24afaa5ca9e353f62ec0783a2
dev-rahim <rahimuddin5915@gmail.com>
2022-10-23 11:23:28 +0600

    added order revew section and show cart addded products data on Ui and add Remove button and implement it's funtionality

--- src/Components/OrderReview/OrderReview.js
+++ src/Components/OrderReview/OrderReview.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
+import { removeFromDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
+import ReviewItem from '../ReviewItems/ReviewItem';
 
 const OrderReview = () => {
     const [products] = useProducts()
-    const [cart] = useCart(products);
+    const [cart, setCart] = useCart(products);
+
+    const handleRemove = (key) => {
+        const remainingItem = cart.filter(item => item.key !== key)
+        setCart(remainingItem)
+        removeFromDb(key)
+    }
     return (
         <div className='Products-container'>
             <div className='Products'>
-                {/* <h1>{products.length}</h1>
-                <h3>{cart.length}</h3> */}
+                {
+                    cart.map(product => <ReviewItem key={product.key} product={product} handleRemove={handleRemove} />)
+                }
             </div>
             <div className='Order-Summery'>
                 <Cart cart={cart} />
---
+++ src/Components/ReviewItems/ReviewItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const ReviewItem = (props) => {
+    const { name, price, img, key, seller, quantity } = props.product;
+
+    return (
+        <div className="product-content">
+            <div>
+                <h3>{name}</h3>
+                <h4>${price}</h4>
+                <p> sold by: {seller}</p>
+                <h5>Quantity:{quantity}</h5>
+                <button
+                    onClick={() => props.handleRemove(key)}
+                    className='btn-addToCart'>Remove</button>
+            </div>
+        </div>
+    );
+};
+
+export default ReviewItem;
